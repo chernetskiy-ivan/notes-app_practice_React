0774e8ad00e7fffdd87ba334bcbfc2b8e8813599
Ivan <ivanchernetskey@mail.ru>
2021-03-01 16:56:31 +0300

    created direction pages for js files of pages. In App.js we see how work component Route for load right page according to the path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
 
 function App() {
   return (
       <BrowserRouter>
           <div className="container">
               <Switch>
-                  <Route />
+                  <Route path='/' exact component={Home}/>
+                  <Route path='/about'  component={About}/>
               </Switch>
           </div>
       </BrowserRouter>
