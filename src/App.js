38c72937cbf5c2f344e1ea399a1c79d4a541d5d1
Ivan <ivanchernetskey@mail.ru>
2021-03-02 12:47:45 +0300

    Prepare component Alert

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {Home} from './pages/Home'
 import {About} from './pages/About'
 import {Navbar} from './Components/Navbar'
+import {Alert} from './Components/Alert'
 
 function App() {
   return (
       <BrowserRouter>
           <Navbar />
           <div className="container">
+              <Alert />
               <Switch>
                   <Route path='/' exact component={Home}/>
                   <Route path='/about'  component={About}/>
