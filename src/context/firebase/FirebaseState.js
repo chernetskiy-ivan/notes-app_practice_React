c61b603efd3b931371c98ac566cbff15b2aae6fe
Ivan <ivanchernetskey@mail.ru>
2021-03-04 19:19:01 +0300

    Modify FirebaseState&Home

--- src/context/firebase/FirebaseState.js
+++ src/context/firebase/FirebaseState.js
@@ -1,6 +1,10 @@
 import React,{useReducer} from 'react'
+import axios from 'axios'
 import {FirebaseContext} from "./firebaseContext";
 import {firebaseReducer} from "./firebaseReducer";
+import {REMOVE_NOTE, SHOW_LOADER} from '../types'
+
+const url = process.env.REACT_APP_DB_URL
 
 export const FirebaseState = ({children}) => {
     const initialState = {
@@ -9,9 +13,36 @@ export const FirebaseState = ({children}) => {
     }
     const [state, dispatch] = useReducer(firebaseReducer, initialState)
 
+    const showLoader = () => dispatch({type: SHOW_LOADER})
+
+    const fetchNotes = async () => {
+        showLoader()
+        const res = await axios.get(`${url}/notes.json`)
+        console.log('fetchNotes', res.data)
+    }
+
+    const addNote = async title => {
+        const note = {
+            title, date: new Date().toJSON()
+        }
+        const res = await axios.post(`${url}/notes.json`, note)
+        console.log('AddNote', res.data)
+    }
+
+    const removeNote = async id => {
+        await axios.delete(`${url}/notes/${id}.json`)
+
+        dispatch({
+            type: REMOVE_NOTE,
+            payload: id
+        })
+    }
+
     return (
         <FirebaseContext.Provider value={{
-
+            showLoader, addNote, fetchNotes, removeNote,
+            loading: state.loading,
+            notes: state.notes
         }}>
             {children}
         </FirebaseContext.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
-import React,{Fragment} from 'react'
+import React,{Fragment, useContext, useEffect} from 'react'
 import {Form} from '../Components/Form'
 import {Notes} from '../Components/Notes'
+import {FirebaseContext} from "../context/firebase/firebaseContext";
 
 export const Home = () => {
-    const notes = new Array(3).fill('').map( ( _, i) => ({id: i, title: `Note ${i +1}`}))
+    const {loading, notes, fetchNotes} = useContext(FirebaseContext)
 
+    useEffect( () => {
+        fetchNotes()
+        // eslint-disable-next-line
+    }, [] )
     return (
         <Fragment>
             <Form/>
